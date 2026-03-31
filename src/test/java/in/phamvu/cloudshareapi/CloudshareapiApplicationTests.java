907ba7f1412df80e19c1caec9e749c872de5ea9f
= <=phamvu65cs@gmail.com>
2026-03-31 22:25:22 +0700

    first commit

---
+++ src/test/java/in/phamvu/cloudshareapi/CloudshareapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.phamvu.cloudshareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudshareapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
